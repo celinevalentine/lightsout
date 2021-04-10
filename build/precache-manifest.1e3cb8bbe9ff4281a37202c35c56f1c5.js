e397a30c80f8840dd0c70dd873b2e9b9658a305a
Celine V <dandan@celines-imac.home>
2021-04-10 00:31:43 -0700

    remove vscode

---
+++ build/precache-manifest.1e3cb8bbe9ff4281a37202c35c56f1c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed7885efcdda0eacabd0c6f3de90b1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf1450e32e8f4ee81593",
+    "url": "/static/css/main.abfc6738.chunk.css"
+  },
+  {
+    "revision": "5d7f81269a1aeb3d5df7",
+    "url": "/static/js/2.82d8df67.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.82d8df67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1450e32e8f4ee81593",
+    "url": "/static/js/main.8b2664dc.chunk.js"
+  },
+  {
+    "revision": "89f2939782a4d7c5cd61",
+    "url": "/static/js/runtime-main.7d0e1bd9.js"
+  }
+]);
